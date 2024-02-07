33481f9e6ec35c961bf84a2c2e42d66ad4c32c6e
Alex <alex.kolev@gmail.com>
2024-02-07 10:52:21 -0500

    Imported Router, Routes, and Route

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my first React Website</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,8 +1,16 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 function navbar() {
   return (
-    <div>navbar</div>
+    <>
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to="/" className='navbar-logo'></Link>
+        Alex Kolev <i className='fab fa-typo3'/>
+      </div>
+    </nav>
+    </>
   )
 }
 
